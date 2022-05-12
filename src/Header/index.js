37f57d24ae9e6b9fb96181757b0bd6de27d8daf3
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-05-12 02:27:07 +0200

    cosmetic changes

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,15 +1,21 @@
 import "./style.css";
 // import Form from "../Form";
 
-const Header = ({onZlotyButtonClick, onDollarButtonClick, onEuroButtonClick}) => (
-<header className="navigation allBoxesProperties">
-<button onClick={onZlotyButtonClick} className="navigation__links"
-  ><span>Złoty polski (PLN)</span></button>
-<button onClick={onEuroButtonClick} className="navigation__links">
-  <span>Euro (EUR)</span></button>
-<button onClick={onDollarButtonClick} className="navigation__links">
-  <span>Dolar amerykański (USD)</span></button>
-</header>
+const Header = ({ onZlotyButtonClick, onDollarButtonClick, onEuroButtonClick }) => (
+  <header>
+    <div className="navigation">
+    
+       <button className="navigation__logo"><img src={require('./logo.png')} width="100px" 
+     height="40px" /></button> 
+    <button onClick={onZlotyButtonClick} className="navigation__links">
+      <span>Złoty polski (PLN)</span></button>
+    <button onClick={onEuroButtonClick} className="navigation__links">
+      <span>Euro (EUR)</span></button>
+    <button onClick={onDollarButtonClick} className="navigation__links">
+      <span>Dolar amerykański (USD)</span></button>
+      </div>
+      
+  </header>
 
 );
 
