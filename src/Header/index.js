caaed40994fa96afdf2424b37638b6021a246466
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-05-22 14:08:41 +0200

    added alt to logo

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,20 +1,19 @@
 import "./style.css";
-// import Form from "../Form";
 
 const Header = ({ onZlotyButtonClick, onDollarButtonClick, onEuroButtonClick }) => (
   <header>
     <div className="navigation">
-    
-       <button className="navigation__logo"><img src={require('./logo.png')} width="100px" 
-     height="40px" /></button> 
-    <button onClick={onZlotyButtonClick} className="navigation__links">
-      <span>Złoty polski (PLN)</span></button>
-    <button onClick={onEuroButtonClick} className="navigation__links">
-      <span>Euro (EUR)</span></button>
-    <button onClick={onDollarButtonClick} className="navigation__links">
-      <span>Dolar amerykański (USD)</span></button>
-      </div>
-      
+
+      <button className="navigation__logo"><img alt="logo" src={require('./logo.png')} width="100px"
+        height="40px"/></button>
+      <button onClick={onZlotyButtonClick} className="navigation__links">
+        <span>Złoty polski (PLN)</span></button>
+      <button onClick={onEuroButtonClick} className="navigation__links">
+        <span>Euro (EUR)</span></button>
+      <button onClick={onDollarButtonClick} className="navigation__links">
+        <span>Dolar amerykański (USD)</span></button>
+    </div>
+
   </header>
 
 );
