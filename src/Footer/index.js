dbbec1d14ffe2c2ff78571c0e880ec9ccf469053
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-05-29 22:28:17 +0200

    added "visited" functionality to links

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,47 +1,72 @@
-import "./style.css";
 
-const Footer = () => (
-  <footer className="footerBox footerBoxProperties" id="contact">
-    <ul className="footerBox__contact">
-      <li className="footerBox__listStyle">Dane kontaktowe:</li>
-      <li className="footerBox__listStyle">Jakub Nowakowski</li>
-      <li className="footerBox__listStyle">
-        e-mail:
-        <a
-          href="mailto:jacob.69.isaac@gmail.com"
-          className="footerBox__decoration"
-        >
-          <span className="footerBox__decoration--textBorder"
-          >jacob.69.isaac@gmail.com</span>
-        </a>
+import "./colorChange.css"
+import React from "react";
+import { Link, LinkText, ListStyle, CopyRight, Contact, Properties, Img } from "./styled.js";
 
-      </li>
-      <li className="footerBox__listStyle">
-        tel:
-        <a href="tel:+48.666(666)666" className="footerBox__decoration">
-          <span className="footerBox__decoration--textBorder"
-          >+48 666-666-666</span></a>
-      </li>
-      <li className="footerBox__listStyle">
 
-        <a
-          target="_blank"
-          rel="noreferrer noopener"
-          href="https://www.facebook.com/nowakowski.malarstwo"
-          className="footerBox__decoration"
-          title="Otwórz w nowej karcie"
-        >
-          <img
-            className="footerBox__decoration--textBorder"
-            src={require('./facebook.png')}
-            alt="Jakub Nowakowski"
-          /></a>
-      </li>
-    </ul>
-    <p className="footerBox__copyRight">
-      ©Copyright 2022 by Jakub Nowakowski - wszystkie prawa zastrzeżone
-    </p>
-  </footer>
-);
+
+
+const Footer = () => {
+
+  const [visitedLink, setVisitedLink] = React.useState(false);
+  const visited = () => {
+    setVisitedLink(true);
+  };
+  const [secondVisitedLink, setSecondVisitedLink] = React.useState(false);
+  const secondVisited = () => {
+    setSecondVisitedLink(true);
+  };
+
+
+  return (
+    <Properties id="contact">
+      <Contact>
+        <ListStyle>Dane kontaktowe:</ListStyle>
+        <ListStyle>Jakub Nowakowski</ListStyle>
+        <ListStyle>
+          e-mail:
+          <Link
+            onClick={visited}
+            className={`${visitedLink === true
+              ? "black"
+              : "blue"
+              }`}
+            href="mailto:jacob.69.isaac@gmail.com"
+          >
+            <LinkText>jacob.69.isaac@gmail.com</LinkText>
+          </Link>
+
+        </ListStyle>
+        <ListStyle>
+          tel:
+          <Link 
+            onClick={secondVisited}
+            className={`${secondVisitedLink === true
+              ? "black"
+              : "blue"
+              }`}
+            href="tel:+48.666(666)666"><LinkText>+48 666-666-666</LinkText></Link>
+        </ListStyle>
+        <ListStyle>
+
+          <Link
+            target="_blank"
+            rel="noreferrer noopener"
+            href="https://www.facebook.com/nowakowski.malarstwo"
+            title="Otwórz w nowej karcie">
+            <Img
+              className="footerBox__decoration--textBorder"
+              src={require('./facebook.png')}
+              alt="Jakub Nowakowski"
+            />
+          </Link>
+        </ListStyle>
+      </Contact>
+      <CopyRight>
+        ©Copyright 2022 by Jakub Nowakowski - wszystkie prawa zastrzeżone
+      </CopyRight>
+    </Properties>
+  )
+};
 
 export default Footer;
