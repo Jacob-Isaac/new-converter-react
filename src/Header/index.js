9f0890aab8ed84e91bd830bbc00e43872b1a055b
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-05-29 22:27:30 +0200

    added styled components to Header

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,19 +1,19 @@
-import "./style.css";
+
+import {Navigation, NavigationLogo, NavigationLinks} from "./styled.js";
 
 const Header = ({ onZlotyButtonClick, onDollarButtonClick, onEuroButtonClick }) => (
   <header>
-    <div className="navigation">
+    <Navigation>
 
-      <button className="navigation__logo"><img alt="logo" src={require('./logo.png')} width="100px"
-        height="40px"/></button>
-      <button onClick={onZlotyButtonClick} className="navigation__links">
-        <span>Złoty polski (PLN)</span></button>
-      <button onClick={onEuroButtonClick} className="navigation__links">
-        <span>Euro (EUR)</span></button>
-      <button onClick={onDollarButtonClick} className="navigation__links">
-        <span>Dolar amerykański (USD)</span></button>
-    </div>
-
+      <NavigationLogo><img alt="logo" src={require('./logo.png')} width="100px"
+        height="40px"/></NavigationLogo>
+      <NavigationLinks onClick={onZlotyButtonClick}>
+        <span>Złoty polski (PLN)</span></NavigationLinks>
+        <NavigationLinks onClick={onEuroButtonClick}>
+        <span>Euro (EUR)</span></NavigationLinks>
+        <NavigationLinks onClick={onDollarButtonClick}>
+        <span>Dolar amerykański (USD)</span></NavigationLinks>
+    </Navigation>
   </header>
 
 );
---
+++ src/Header/styled.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+
+export  const Navigation = styled.div`
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        box-shadow: 0.1px 0.1px 10px;
+        background-color: rgb(255 255 255 / 80%);
+        width: 100%;
+        min-width: 250px;
+    
+    @media (max-width: 968px) {
+        box-shadow: 5px 5px 10px;
+        background-color: rgb(255 255 255 / 80%);
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+`;
+
+export  const NavigationLogo = styled.button`    
+        background-color: transparent;
+        border: none;
+`;
+
+export  const NavigationLinks = styled.button` 
+        background-color: transparent;
+        border: none;
+        margin: 20px;
+        padding: 15px;
+        font-family: "Montserrat", cursive, sans-serif;
+        text-decoration: none;
+        color: black;
+        font-weight: bolder;
+        transition: all 0.01s ease-in-out;
+
+    &:hover {
+        color: rgb(158, 158, 158);
+        cursor: pointer;
+    }
+    &:active {
+        color: rgb(158, 158, 158);
+        transform: scale(0.8);
+    } 
+`;
